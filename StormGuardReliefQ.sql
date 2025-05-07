c812a9da9d7b0c562ca12d19b61429fbe5ed8389
niveprabhu <55413823+niveprabhu@users.noreply.github.com>
2025-05-06 20:04:28 -0500

    Update StormGuardReliefQ.sql

--- StormGuardReliefQ.sql
+++ StormGuardReliefQ.sql
@@ -1,20 +1,35 @@
-USE StormGuardReliefDB;
+USE StormGuardReliefDB2;
 
-#Queries By Nivedita Prabhu
+-- Queries By Nivedita Prabhu
+
+# Basic 1: one sentence description in the comment 
 SELECT Name 
 	FROM Hurricane;
+# Basic 2: one sentence description in the comment 
 
+# Advanced 1 : one sentence description in the comment 
 SELECT v.First_Name, v.Last_Name, r.Title
 	FROM Volunteer v
 	JOIN Volunteer_Relief vr ON v.Volunteer_id = vr.Volunteer_id
 	JOIN Relief r ON vr.Relief_id = r.Relief_id
 	WHERE r.Active = 'Active';
+    
+# Advanced 2: one sentence description in the comment
 
-#Queries By Nicholas Scott Marshall
+# Advanced 3: one sentence description in the comment  
+
+/*_______________________________________________________________________*/
+
+-- Queries By Nicholas Scott Marshall
+
+# Basic 1: one sentence description in the comment 
 SELECT *
 	FROM Logistics
 	WHERE Delivery_Status = 'Ongoing';
 
+# Basic 2: one sentence description in the comment 
+
+# Advanced 1 : one sentence description in the comment 
 SELECT l.Name AS Location,
 	COUNT(r.Relief_id) AS Total_Relief_Requests,
 	GROUP_CONCAT(r.Title SEPARATOR ', ') AS Relief_Titles
@@ -22,9 +37,16 @@ SELECT l.Name AS Location,
 	JOIN Country_Requests_Relief cr ON l.Country_id = cr.Country_id
 	JOIN Relief r ON cr.Relief_id = r.Relief_id
 	GROUP BY l.Name;
+    
+# Advanced 2: one sentence description in the comment
 
+# Advanced 3: one sentence description in the comment  
 
-#Queries By Ayodeji Joseph Adeogun
+/*_______________________________________________________________________*/
+
+-- Queries By Ayodeji Joseph Adeogun
+
+# Basic 1: one sentence description in the comment 
 SELECT 
     r.Relief_id, 
     r.Title, 
@@ -35,7 +57,10 @@ SELECT
     FROM Relief r
 	JOIN Logistics l ON r.Relief_id = l.Relief_id
 	WHERE r.Active = 'Active';
-		
+
+# Basic 2: one sentence description in the comment 
+
+# Advanced 1: one sentence description in the comment 	
 SELECT 
     r.Title AS Relief_Title,
     r.Type,
@@ -49,3 +74,7 @@ SELECT
 	JOIN Logistics l ON r.Relief_id = l.Relief_id
 	JOIN Volunteer v ON v.Assigned_Region IS NOT NULL
 	GROUP BY r.Title, r.Type, r.Active, l.Supply_Status, l.Delivery_Status, v.Assigned_Region;
+    
+# Advanced 2: one sentence description in the comment
+
+# Advanced 3: one sentence description in the comment  
